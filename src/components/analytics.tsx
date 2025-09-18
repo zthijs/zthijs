201be511aaf0fbac97b10ed910c07812c9cff6be
zthijs <contact@zthijs.dev>
2025-09-18 14:35:11 +0200

    fix: gallery grid

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,3 +1,5 @@
 import Script from "next/script";
 
-export const Analytics = () => (<Script async src="https://scripts.simpleanalyticscdn.com/latest.js" />);
+export const Analytics = () => (
+  <Script async src="https://scripts.simpleanalyticscdn.com/latest.js" />
+);
