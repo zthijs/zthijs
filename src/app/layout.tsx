c4ccea9728fe43f26e71bb65aad275037789f84d
zthijs <zthijs@outlook.com>
2026-04-15 21:57:59 +0200

    refactor: standardize import styles and formatting across components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,57 +1,57 @@
-import type { Metadata } from "next";
-import { ThemeProvider } from "next-themes";
-import { Analytics } from "@/components/analytics";
-import { Footer } from "@/components/ui/footer";
+import type { Metadata } from 'next';
+import { ThemeProvider } from 'next-themes';
+import { Analytics } from '@/components/analytics';
+import { Footer } from '@/components/ui/footer';
 
-import "react-photo-view/dist/react-photo-view.css";
-import "@/assets/styles/globals.css";
+import 'react-photo-view/dist/react-photo-view.css';
+import '@/assets/styles/globals.css';
 
 export const metadata: Metadata = {
-  title: "Thijs",
-  description: "Full stack software & media creation by Thijs",
-  keywords: [
-    "software development",
-    "web development",
-    "media creation",
-    "full stack development",
-    "devops",
-    "cloud computing",
-    "automation",
-    "software engineering",
-    "nextjs",
-    "react",
-    "typescript",
-    "nodejs",
-    "tailwindcss",
-    "zthijs",
-    "zthijs.dev",
-    "zthijs.nl",
-  ],
+    title: 'Thijs',
+    description: 'Full stack software & media creation by Thijs',
+    keywords: [
+        'software development',
+        'web development',
+        'media creation',
+        'full stack development',
+        'devops',
+        'cloud computing',
+        'automation',
+        'software engineering',
+        'nextjs',
+        'react',
+        'typescript',
+        'nodejs',
+        'tailwindcss',
+        'zthijs',
+        'zthijs.dev',
+        'zthijs.nl',
+    ],
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://rsms.me/" />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-      </head>
-      <body className="selection:bg-base-200 selection:text-base-900 antialiased">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          storageKey="theme"
-          enableSystem
-        >
-          <main className="grow">{children}</main>
-          <Footer />
-        </ThemeProvider>
-        <Analytics />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <head>
+                <link rel="preconnect" href="https://rsms.me/" />
+                <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+            </head>
+            <body className="selection:bg-base-200 selection:text-base-900 antialiased">
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    storageKey="theme"
+                    enableSystem
+                >
+                    <main className="grow">{children}</main>
+                    <Footer />
+                </ThemeProvider>
+                <Analytics />
+            </body>
+        </html>
+    );
 }
--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,20 +1,20 @@
-import type { MetadataRoute } from "next";
-import { root } from "@/constants/site";
+import type { MetadataRoute } from 'next';
+import { root } from '@/constants/site';
 
 export const revalidate = false;
 
 export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-      },
-      {
-        userAgent: "ia_archiver",
-        disallow: "/",
-      },
-    ],
-    sitemap: `${root}/sitemap.xml`,
-  };
+    return {
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+            {
+                userAgent: 'ia_archiver',
+                disallow: '/',
+            },
+        ],
+        sitemap: `${root}/sitemap.xml`,
+    };
 }
