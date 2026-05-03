fbd6dd6d8020689f1951a01e24910241c0c534df
zthijs <zthijs@outlook.com>
2026-05-03 09:30:18 +0200

    feat: update deployment workflow to retrieve Pages URL and configure analytics script

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -3,9 +3,7 @@ import Script from 'next/script';
 export const Analytics = () => (
   <Script
     defer
-    src="https://static.cloudflareinsights.com/beacon.min.js"
-    data-cf-beacon={JSON.stringify({
-      token: process.env.NEXT_PUBLIC_CF_BEACON_TOKEN,
-    })}
+    data-domain={process.env.NEXT_PUBLIC_PAGES_DOMAIN}
+    src={process.env.NEXT_PUBLIC_VINCE_SCRIPT_URL}
   />
 );
