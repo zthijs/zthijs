7439b36d2e70702143af2e4fbcb87df9d45abd93
zthijs <thijs.zijlstra@ah.nl>
2026-06-21 13:59:14 +0200

    Add new SVG icons for Hibernate, Nginx, Postman, Prisma, Redis, Vite, and Vitest

--- src/constants/stack.ts
+++ src/constants/stack.ts
@@ -5,20 +5,26 @@ export interface StackItem {
 }
 
 export const stack: StackItem[] = [
+  // Languages
+  {
+    image: '/icons/js.svg',
+    title: 'JavaScript',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+  },
   {
     image: '/icons/ts.svg',
     title: 'TypeScript',
     link: 'https://www.typescriptlang.org/',
   },
   {
-    image: '/icons/java.svg',
-    title: 'Java',
-    link: 'https://www.java.com/',
+    image: '/icons/html.svg',
+    title: 'HTML',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
   },
   {
-    image: '/icons/kotlin.svg',
-    title: 'Kotlin',
-    link: 'https://kotlinlang.org/',
+    image: '/icons/css.svg',
+    title: 'CSS',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
   },
   {
     image: '/icons/python.svg',
@@ -26,19 +32,20 @@ export const stack: StackItem[] = [
     link: 'https://www.python.org/',
   },
   {
-    image: '/icons/react.svg',
-    title: 'React',
-    link: 'https://react.dev/',
+    image: '/icons/kotlin.svg',
+    title: 'Kotlin',
+    link: 'https://kotlinlang.org/',
   },
+  // Frameworks
   {
     image: '/icons/nextjs.svg',
     title: 'Next.js',
     link: 'https://nextjs.org/',
   },
   {
-    image: '/icons/angular.svg',
-    title: 'Angular',
-    link: 'https://angular.io/',
+    image: '/icons/react.svg',
+    title: 'React',
+    link: 'https://react.dev/',
   },
   {
     image: '/icons/spring.svg',
@@ -46,14 +53,20 @@ export const stack: StackItem[] = [
     link: 'https://spring.io/',
   },
   {
-    image: '/icons/graphql.svg',
-    title: 'GraphQL',
-    link: 'https://graphql.org/',
-  },
-  {
     image: '/icons/tailwind.svg',
     title: 'Tailwind CSS',
     link: 'https://tailwindcss.com/',
+  },
+  // Build & Test
+  {
+    image: '/icons/vite.svg',
+    title: 'Vite',
+    link: 'https://vite.dev/',
+  },
+  {
+    image: '/icons/vitest.svg',
+    title: 'Vitest',
+    link: 'https://vitest.dev/',
   },
   {
     image: '/icons/jest.svg',
@@ -65,21 +78,7 @@ export const stack: StackItem[] = [
     title: 'Cypress',
     link: 'https://www.cypress.io/',
   },
-  {
-    image: '/icons/docker.svg',
-    title: 'Docker',
-    link: 'https://www.docker.com/',
-  },
-  {
-    image: '/icons/kubernetes.svg',
-    title: 'Kubernetes',
-    link: 'https://kubernetes.io/',
-  },
-  {
-    image: '/icons/nodejs.svg',
-    title: 'Node.js',
-    link: 'https://nodejs.org/',
-  },
+  // Databases & ORM
   {
     image: '/icons/mongodb.svg',
     title: 'MongoDB',
@@ -96,28 +95,75 @@ export const stack: StackItem[] = [
     link: 'https://www.postgresql.org/',
   },
   {
-    image: '/icons/vscode.svg',
-    title: 'VS Code',
-    link: 'https://code.visualstudio.com/',
+    image: '/icons/redis.svg',
+    title: 'Redis',
+    link: 'https://redis.io/',
   },
+  {
+    image: '/icons/graphql.svg',
+    title: 'GraphQL',
+    link: 'https://graphql.org/',
+  },
+  {
+    image: '/icons/prisma.svg',
+    title: 'Prisma',
+    link: 'https://www.prisma.io/',
+  },
+  {
+    image: '/icons/drizzle.svg',
+    title: 'Drizzle ORM',
+    link: 'https://orm.drizzle.team/',
+  },
+  {
+    image: '/icons/hibernate.svg',
+    title: 'Hibernate',
+    link: 'https://hibernate.org/',
+  },
+  // DevOps & Cloud
+  {
+    image: '/icons/docker.svg',
+    title: 'Docker',
+    link: 'https://www.docker.com/',
+  },
+  {
+    image: '/icons/kubernetes.svg',
+    title: 'Kubernetes',
+    link: 'https://kubernetes.io/',
+  },
+  {
+    image: '/icons/nginx.svg',
+    title: 'Nginx',
+    link: 'https://nginx.org/',
+  },
+  {
+    image: '/icons/azure.svg',
+    title: 'Azure',
+    link: 'https://azure.microsoft.com/',
+  },
+  {
+    image: '/icons/nodejs.svg',
+    title: 'Node.js',
+    link: 'https://nodejs.org/',
+  },
+  // Tools
   {
     image: '/icons/idea.svg',
     title: 'IntelliJ IDEA',
     link: 'https://www.jetbrains.com/idea/',
   },
   {
-    image: '/icons/js.svg',
-    title: 'JavaScript',
-    link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+    image: '/icons/vscode.svg',
+    title: 'VS Code',
+    link: 'https://code.visualstudio.com/',
   },
   {
-    image: '/icons/html.svg',
-    title: 'HTML',
-    link: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
+    image: '/icons/postman.svg',
+    title: 'Postman',
+    link: 'https://www.postman.com/',
   },
   {
-    image: '/icons/css.svg',
-    title: 'CSS',
-    link: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+    image: '/icons/github.svg',
+    title: 'GitHub',
+    link: 'https://github.com/',
   },
 ];
